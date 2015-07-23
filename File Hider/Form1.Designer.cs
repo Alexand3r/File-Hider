d57807ffa712e9f44fcd51927c97730dcb110c50
Alexand3r <andrei.tudor27@yahoo.com>
2015-07-23 23:57:45 +0200

    Initial Commit

---
+++ File Hider/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace File_Hider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "File to hide";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(184, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Image";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 103);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(279, 46);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "DO IT!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 77);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(184, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "Save Location";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(217, 77);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 161);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "File Hider";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ File Hider/Form1.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Hider
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            textBox1.Text = openFileDialog1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            textBox2.Text = openFileDialog2.FileName;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+                        //Archive file
+            string the_rar;
+            RegistryKey the_Reg;
+            object the_Obj;
+            string the_Info;
+            ProcessStartInfo the_StartInfo;
+            Process the_Process;
+            try
+            {
+                the_Reg = Registry.ClassesRoot.OpenSubKey(@"Applications\WinRAR.exe\Shell\Open\Command");
+                the_Obj = the_Reg.GetValue("");
+                the_rar = the_Obj.ToString();
+                the_Reg.Close();
+                the_rar = the_rar.Substring(1, the_rar.Length - 7);
+                the_Info = string.Format(" a " +" "+ "\""+textBox1.Text+".rar\" " + " " + "\""+@textBox1.Text+"\"");
+                label1.Text = the_Info;
+                the_StartInfo = new ProcessStartInfo();
+                
+                the_StartInfo.FileName = the_rar;
+                the_StartInfo.Arguments = the_Info;
+                the_StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                the_StartInfo.CreateNoWindow = true;
+               // the_StartInfo.WorkingDirectory = @"C:\test\";
+                the_Process = new Process();
+                the_Process.StartInfo = the_StartInfo;
+                the_Process.Start();
+                MessageBox.Show("success");
+            }
+            catch
+            {
+              
+            }
+
+                        //Merge Files
+            string cmd = string.Format(@"/c copy /b " + "\"" + textBox2.Text.ToString() + "\"" + " + " + "\"" + textBox1.Text + ".rar\" " + textBox3.Text + "hidden.jpg");
+                        
+                        ProcessStartInfo psi = new ProcessStartInfo("cmd.exe");
+                        psi.Arguments = cmd;
+                        
+                        psi.RedirectStandardOutput = true;
+                        psi.UseShellExecute = false;
+                        psi.CreateNoWindow = true;
+                        Process p = Process.Start(psi);
+                        
+                        string result = p.StandardOutput.ReadToEnd();            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+       
+    }
+}
